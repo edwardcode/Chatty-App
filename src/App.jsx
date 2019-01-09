25390cfbe33eaa6c2adf6cd8591c27cc925c58ff
EDWARD YANG <eddiecody888@gmail.com>
2019-01-09 21:48:13 +0000

    the user counter added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,55 @@
 import React, {Component} from 'react';
+import MessageList from './MessageList.jsx';
+import ChatBar from './ChatBar.jsx';
+
+
+function NavBar({counter}){
+  return (<nav className="navbar">
+            <a href="/" className="navbar-brand">Chatty</a>
+             <span className="navbar-userscount"> {counter} </span>
+          </nav>
+          )
+};
+
+
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      currentUser:'Anonymous',
+      message:[],
+      counter:1,
+
+      };
+
+  }
+
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({loading:false});
+
+    },3000)
+  }
+
+
+
+
   render() {
+     const countUserNb= `${this.state.counter} user${(this.state.counter > 1) ? 's' : ''} online`
+
     return (
-      <h1>Hello React :)</h1>
+      <div>
+         <NavBar counter={countUserNb}  />
+
+         <MessageList   />
+
+         <ChatBar
+
+
+         />
+
+      </div>
     );
   }
 }
